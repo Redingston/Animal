40b8c99b6c8e7fc89c442ac1e1d311fd5bf9374d
Redigston <kondratukktc@gmail.com>
2019-12-18 22:22:30 +0200

    add user profile

--- profile.php
+++ profile.php
@@ -5,27 +5,33 @@ include_once "_navbar.php";
 <?php include_once "connect_db.php"; ?>
     <div class="container">
         <div class="row">
+        <h1>Your profile</h1>
             <table class="table">
                 <thead>
                 <tr>
                     <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
+                    <th scope="col">User</th>
+                    <th scope="col">Email</th>
+                    
                 </tr>
                 </thead>
                 <tbody>
-
-
-                <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                </tr>
+                <?php
+                $query = "SELECT UserName,Email,Photo FROM tb_user";
+                $sth = $dbh->prepare($query);
+                $sth->execute();
+                while ($row = $sth->fetch(PDO::FETCH_ASSOC)) {
+                    echo '
+                    <tr>
+                    <th scope="row"><img src ="'.$row["Photo"].'" width="150"/></th>
+                        <td>' . $row["UserName"] . '</td>
+                        <td>' . $row["Email"] . '</td>
+                    </tr>
+                    ';
+                }
+                ?>
                 </tbody>
             </table>
-           
         </div>
     </div>
 
